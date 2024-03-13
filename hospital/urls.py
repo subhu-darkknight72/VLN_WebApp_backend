c5cd97fb7aa4cb2111ed3cf4cb48e55fd6c94f7f
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2024-03-13 16:16:20 +0530

    Push the offline version of the app

--- hospital/urls.py
+++ hospital/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('reset/', views.reset, name='reset'),
+    path('reset/', views.reset, name='hospitalReset'),
     path('performAction/', views.performAction, name='performAction'),
     path('', views.index, name='hospitalIndex'),
 ]
